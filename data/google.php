71bab45ecdeeda9170c3d43b3ccb908ecbdd303c
David Heyman <dave@axismaps.com>
2013-05-13 13:54:55 +0100

    Switched to absolute links for includes in Google to run with collect news in Crontab

--- data/google.php
+++ data/google.php
@@ -1,12 +1,12 @@
 <?php
-	require_once '../lib/google-api-php-client/Google_Client.php';
-	require_once '../lib/google-api-php-client/contrib/Google_FusiontablesService.php';
+	require_once '/var/www/map/lib/google-api-php-client/Google_Client.php';
+	require_once '/var/www/map/lib/google-api-php-client/contrib/Google_FusiontablesService.php';
 
 	/* Define all constants */
 	const CLIENT_ID = '51995355460-d4bkpsf6350ggm47n17mqeiam2blmia8.apps.googleusercontent.com';
 	const FT_SCOPE = 'https://www.googleapis.com/auth/fusiontables';
 	const SERVICE_ACCOUNT_NAME = '51995355460-d4bkpsf6350ggm47n17mqeiam2blmia8@developer.gserviceaccount.com';
-	const KEY_FILE = '../lib/google-api-php-client/305961c9f49723cf626fe798fae641c2c6d3e783-privatekey.p12';
+	const KEY_FILE = '/var/www/map/lib/google-api-php-client/305961c9f49723cf626fe798fae641c2c6d3e783-privatekey.p12';
 	
 	function google_auth()
 	{
